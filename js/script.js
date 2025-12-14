ed1795ccc065e7e7137dabe8e4a3952354a083c0
bagus novaldi <bagusnovaldi@example.com>
2025-12-14 22:07:03 +0700

    update index html

--- js/script.js
+++ js/script.js
@@ -16,11 +16,15 @@ document.querySelector('#search-button').onclick = (e) => {
   e.preventDefault();
 };
 
-//klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+//klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector('#search-button');
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
