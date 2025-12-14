2a9f1abae4a8ce3abb3079a84d72e47089bedc89
bagus novaldi <bagusnovaldi@example.com>
2025-12-14 21:28:14 +0700

    update index html

--- js/script.js
+++ js/script.js
@@ -10,8 +10,10 @@ document.querySelector("#hamburger-menu").onclick = () => {
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
 
-document.querySelector('search-button').onclick = () => {
+document.querySelector('#search-button').onclick = (e) => {
   searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
 };
 
 //klik di luar sidebar untuk menghilangkan nav
